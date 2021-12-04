f81759e13ce8b04a42ee509f4ae4f63c5d8a1937
ripal <ripal@LAPTOP-PJ1O34EU>
2021-12-04 22:17:13 +0000

    Added if statement

---
+++ 01 First Solution/02Loops/LoopsClass.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Loops
+
+{
+    public class LoopsClass
+    {
+        public static void Main()
+        {
+            
+            string[] AnimalsArray = { "dog", "cat", "elephant", "zebra" };
+            Console.WriteLine("Enter an number between 0 and 3");
+            string OptionString = Console.ReadLine();
+            bool Success = int.TryParse(OptionString, out int OptionInt);
+
+            if (Success.Equals(true))
+            {
+
+                if (OptionInt >= 0 & OptionInt <= 3)
+                {
+                    Console.WriteLine("The Animal you picked is " + AnimalsArray[OptionInt]);
+                }
+                else
+                {
+                    Console.WriteLine("You Picked an invalid choice");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a number");
+            }
+
+        }
+
+    }
+
+}
+
