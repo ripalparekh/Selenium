53cd77c6c9ffdf2844205159e1e076e0d8e11785
ripal <ripal@LAPTOP-PJ1O34EU>
2021-12-04 23:42:22 +0000

    looped

--- 01 First Solution/02Loops/LoopsClass.cs
+++ 01 First Solution/02Loops/LoopsClass.cs
@@ -8,42 +8,48 @@ namespace Loops
     {
         public static void Main()
         {
-            
-            string[] AnimalsArray = { "dog", "cat", "elephant", "zebra" };
-            string[] Cars = { "Fiat", "Lexus", "Mercedes", "Ferarri" };
-            Console.WriteLine("Enter an number between 0 and 3");
-            string OptionString = Console.ReadLine();
-            bool Success = int.TryParse(OptionString, out int OptionInt);
-
-            if (Success.Equals(true))
+            bool RunAgain = true;
+            while (RunAgain == true)
             {
+                Console.Clear();
+                string[] AnimalsArray = { "dog", "cat", "elephant", "zebra" };
+                string[] Cars = { "Fiat", "Lexus", "Mercedes", "Ferarri" };
+                Console.WriteLine("Enter an number between 0 and 3");
+                string OptionString = Console.ReadLine();
+                bool Success = int.TryParse(OptionString, out int OptionInt);
 
-                if (OptionInt >= 0 & OptionInt <= 3)
+                if (Success.Equals(true))
                 {
-                    Console.WriteLine("The Animal you picked is " + AnimalsArray[OptionInt]);
-                    Console.WriteLine("You could have picked from these: ");
-                    PrintAnimals(AnimalsArray);
+
+                    if (OptionInt >= 0 & OptionInt <= 3)
+                    {
+                        Console.WriteLine("The Animal you picked is " + AnimalsArray[OptionInt]);
+                        Console.WriteLine("You could have picked from these: ");
+                        PrintAnimals(AnimalsArray);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Picked an invalid choice");
+                    }
+
                 }
                 else
                 {
-                    Console.WriteLine("You Picked an invalid choice");
+                    Console.WriteLine("You did not enter a number");
                 }
 
-            }
-            else
-            {
-                Console.WriteLine("You did not enter a number");
-            }
+                PrintCars(Cars);
+                AnimalCar(Cars, AnimalsArray);
 
-            PrintCars(Cars);
-
+                RunAgain = AskToStop();
+            }
         }
         #region Methods       
         public static void PrintAnimals(string[] AnimalsList)
         {
             foreach (string Animal in AnimalsList)
             {
-                Console.WriteLine(Animal); 
+                Console.WriteLine(Animal);
             }
         }
 
@@ -54,7 +60,33 @@ namespace Loops
                 Console.WriteLine("The Car is " + CarList[count]);
             }
         }
+
+        public static void AnimalCar(string[] CarList, string[] Animals)
+        {
+            var RandAnimal = new Random();
+            var RandCar = new Random();
+
+            Console.WriteLine("the Animal " + Animals[RandAnimal.Next(Animals.Length)] +
+                " matches with the " + CarList[RandCar.Next(CarList.Length)]);
+        }
+        
+        public static bool AskToStop()
+        {
+            Console.WriteLine("Press Q to quit or any other key to run again");
+            char Answer = Console.ReadKey().KeyChar;
+            if ((Answer is (char)'q') || (Answer is (char)'Q'))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
         #endregion
+        
     }
 
 }
